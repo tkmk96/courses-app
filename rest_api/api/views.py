095b786587e333f7ee5b142a92545fa38cdc0c61
tkmk96 <tkmk96@gmail.com>
2018-11-14 17:56:08 +0100

    added rest framework

--- rest_api/api/views.py
+++ rest_api/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from .models import Category, Course, User, CourseUser, RecommendationPeopleBuy, RecommendationSimilarCourse, RecommendationForUser
+from rest_framework import viewsets
+from .serializers import CategorySerializer, CourseSerializer, UserSerializer
 
 # Create your views here.
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('-name')
+    serializer_class = CategorySerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all().order_by('-name')
+    serializer_class = CourseSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-name')
+    serializer_class = UserSerializer
--- rest_api/rest_api/urls.py
+++ rest_api/rest_api/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url, include
 from django.urls import path
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'category', views.CategoryViewSet)
+router.register(r'course', views.CourseViewSet)
+router.register(r'user', views.UserViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
