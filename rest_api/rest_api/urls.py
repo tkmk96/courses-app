d69c2d56c73889c5a7373194ce7c08bc4c6a1ff7
martinsabaka <mart.sabaka@gmail.com>
2018-12-04 14:56:57 +0100

    added endpoint for trending, filter by count not working yet

--- rest_api/rest_api/urls.py
+++ rest_api/rest_api/urls.py
@@ -22,7 +22,7 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'course', views.CourseViewSet, base_name='course')
 router.register(r'user', views.UserViewSet)
-#router.register(r'trending', views.TrendingViewSet, base_name='trending')
+router.register(r'trending', views.TrendingViewSet, base_name='trending')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
